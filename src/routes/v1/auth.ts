0885c4692954d803387f9ac6e1b41535c0834915
MBashov <galacticos.mb@gmail.com>
2025-07-14 13:48:54 +0300

    Validation error middleware

--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -5,10 +5,17 @@ import { Router } from 'express';
 import register from '../../controllers/v1/auth/register.ts';
 import login from '../../controllers/v1/auth/login.ts';
 
+
+//* Middlewares
+import validationError from '../../middlewares/validationError.ts';
+
+//* Utils
+import { registerValidator } from '../../utils/userValidators.ts';
+
 const authRouter = Router();
 
-authRouter.post('/register', register);
-authRouter.post('/login', login);
+authRouter.post('/register', registerValidator, validationError, register);
+authRouter.post('/login',  validationError, login);
 
 
 export default authRouter;
