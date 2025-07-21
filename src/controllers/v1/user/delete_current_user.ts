4061e4c7da52ebca70f605fb4115b00faac09f55
MBashov <galacticos.mb@gmail.com>
2025-07-21 15:27:23 +0300

    Delete user's blog banners when deleting a user

--- src/controllers/v1/user/delete_current_user.ts
+++ src/controllers/v1/user/delete_current_user.ts
@@ -1,8 +1,12 @@
+//* Modules
+import { v2 as cloudinary } from 'cloudinary';
+
 //* Custom modules
 import { logger } from "../../../lib/winston.ts";
 
 //* Models
 import User from "../../../models/user.ts";
+import Blog from '../../../models/blog.ts';
 
 //* Types
 import type { Response } from "express";
@@ -12,6 +16,21 @@ const deleteCurrentUser = async (req: CustomRequest, res: Response): Promise<voi
     const userId = req.userId;
     
     try {
+        const blogs = await Blog.find({ author: userId }).select('banner.publicId').lean().exec(); 
+        
+        const publicIds = blogs.map(({ banner }) => banner.publicId);
+
+        await cloudinary.api.delete_resources(publicIds);
+        logger.info('Multiple blog banners deleted from cloudinary', {
+            publicIds
+        });
+
+        await Blog.deleteMany({ author: userId });
+        logger.info('Multiple blogs deleted', {
+            userId,
+            blogs,
+        });
+
         await User.deleteOne({ _id: userId });
 
         logger.info('A user account has been deleted', userId);
